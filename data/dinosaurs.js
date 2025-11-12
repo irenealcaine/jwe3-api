1351b44ac1ee073c5c1194c6d93bf67f43ea4b73
irenealcaine <101673351+irenealcaine@users.noreply.github.com>
2025-11-12 14:14:34 +0000

    images & docs

--- data/dinosaurs.js
+++ data/dinosaurs.js
@@ -2,13 +2,13 @@ export const dinosaurs = [
   {
     id: 1,
     name: "Tyrannosaurus Rex",
-    image: "http://localhost:3000/images/trex.webp",
-    description: "Un gran carnívoro del Cretácico tardío, conocido por su poderosa mandíbula.",
+    image: "/images/trex.png",
+    description: "Un gran carnívoro del Cretácico tardío.",
   },
   {
     id: 2,
     name: "Triceratops",
-    image: "http://localhost:3000/images/triceratops.webp",
-    description: "Herbívoro con tres cuernos y un gran escudo óseo en la cabeza.",
+    image: "/images/triceratops.png",
+    description: "Herbívoro con tres cuernos y un gran escudo óseo.",
   },
 ];
